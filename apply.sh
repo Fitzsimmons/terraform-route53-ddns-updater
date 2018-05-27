06340c559ccc95df9cdc059baba49b62452bda65
Justin Fitzsimmons <justin@fitzsimmons.ca>
2018-05-26 20:03:02 -0400

    Ruggedize apply.sh

--- apply.sh
+++ apply.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" > /dev/null
+
 terraform init -get=true -get-plugins=true -upgrade=true
 terraform apply -var-file variables.tfvars.json -auto-approve
+
+popd > /dev/null
